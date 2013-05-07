469ed8c8bfbec5921e5f465e1b1ddb588399e206
apburnes <apburnes@gmail.com>
2013-05-07 23:31:01 +0000

    Created plot_export module and img directory to clear response_units.py

---
+++ code/plot_export.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import os
+
+##### test plot #####
+
+def savePlot(plotted, graph):
+    
+    directory = "img/"
+    graphic = os.path.join(directory, graph)
+    
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.plot(plotted)
+
+    ### default graphic is png format
+    fig.savefig(graphic)
+
+if __name__ == '__main__':
+    
+    test_plot = np.arange(25)
+    test_graph = "mytest"
+    
+    savePlot(test_plot, test_graph)
