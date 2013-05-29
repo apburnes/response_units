1ef232e448b59e054dae05157d64450167ee8652
Andrew Burnes <apburnes@gmail.com>
2013-05-29 14:47:37 -0700

    Added raster data and simple plotting to see data

---
+++ code/raster_management.py
@@ -0,0 +1,18 @@
+
+""" Handle Raster Data for Overlay and Segmentation """
+
+#Import modules
+import numpy as np
+from numpy import ma
+from numpy.random import randn
+import pandas as pd
+import matplotlib.pyplot as plt
+from osgeo import gdal, ogr
+import os
+
+def myTest(x):
+    y = np.arange(x)
+    return y
+    
+if __name__ == '__main__':
+    myTest()
