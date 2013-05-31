d17970e4de948bc3673b8ac3729bf02f749a1a24
Andrew Burnes <apburnes@gmail.com>
2013-05-31 15:58:32 -0700

    created an output directory for stacked rasters

--- code/raster_management.py
+++ code/raster_management.py
@@ -4,16 +4,27 @@
 #Import modules
 import numpy as np
 from numpy import ma
+import pysal as ps
 from numpy.random import randn
 import pandas as pd
 import matplotlib.pyplot as plt
 from osgeo import gdal, ogr
-import os
+from osgeo.gdalconst import *
+import os, sys, time
 
-def myTest(x):
-    """Testing the methods of the project"""
-    y = np.arange(x)
-    return y
+
+def list_rasters(input_dir,file_type):
+    """Creates a list of rasters from a defined directory and selected filetype."""
+   
+    raster_list = []
+    
+    for data in os.listdir(input_dir):
+        if data.endswith(file_type):
+            raster_list.append(data)
+    return raster_list
+
+def bands_to_array(raster)
+
     
 if __name__ == '__main__':
     myTest()
